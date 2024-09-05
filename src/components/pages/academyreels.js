6ffd122d1c0c80a63e411eabc9326b07674df035
Reeshabh Jagetiya <reeshabh.rethinkingweb@gmail.com>
2024-09-05 19:07:28 +0530

    class added

--- src/components/pages/academyreels.js
+++ src/components/pages/academyreels.js
@@ -35,50 +35,44 @@ export default function Academyreels() {
       <div className="reelslider">
         <div className="viewmaster">
           <div className="" onClick={()=>setAutoplay(false)}>
-            <Swiper
-              modules={[
-                Navigation,
-                A11y,
-                Autoplay,
-                EffectFade,
-                EffectCoverflow,
-              ]}
-              onSwiper={(swiper) => console.log(swiper)}
-              onSlideChange={handleSlideChange}
-              autoplay={false}
-              loop={false}
-              effect="slide"
-              speed={6000}
-              spaceBetween={0}
-              breakpoints={{
-                300: {
-                  slidesPerView: 2,
-                  spaceBetween: 0,
-                },
-                920: {
-                  slidesPerView: 3,
-                  spaceBetween: 0,
-                },
-                1340: {
-                  slidesPerView: 5,
-                  spaceBetween: 0,
-                },
-              }}
-              freeMode="true"
-              grabCursor="true"
-            >
-              {slide_img.map((url, i) => {
-                return (
-                  <SwiperSlide key={i}>
-                    <div className="doteffect">
-                      <div className="carousel-cell sepiaEffect">
-                      <VideoPlayer key={i} src={"https://share.applykart.co/"+url} />
-                      </div>
-                    </div>
-                  </SwiperSlide>
-                );
-              })}
-            </Swiper>
+          <Swiper
+  modules={[Navigation, A11y, Autoplay, EffectFade, EffectCoverflow]}
+  onSwiper={(swiper) => console.log(swiper)}
+  onSlideChange={handleSlideChange}
+  autoplay={false}
+  loop={false}
+  effect="slide"
+  speed={6000}
+  spaceBetween={0}
+  breakpoints={{
+    300: {
+      slidesPerView: 2,
+      spaceBetween: 0,
+    },
+    920: {
+      slidesPerView: 3,
+      spaceBetween: 0,
+    },
+    1340: {
+      slidesPerView: 5,
+      spaceBetween: 0,
+    },
+  }}
+  freeMode="true"
+  grabCursor="true"
+>
+  {slide_img.map((url, i) => {
+    return (
+      <SwiperSlide key={i} className={`academy-${i + 1}`}>
+        <div className="doteffect">
+          <div className="carousel-cell sepiaEffect video-academy">
+            <VideoPlayer key={i} src={`https://share.applykart.co/${url}`} />
+          </div>
+        </div>
+      </SwiperSlide>
+    );
+  })}
+</Swiper>
           </div>
         </div>
       </div>
